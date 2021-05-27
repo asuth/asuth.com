61bab44f378b9358f96bde3d6e49bf46d163252d
Andrew Sutherland <asuth@asuth.com>
2021-05-27 16:22:00 -0700

    updated about content

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="PageHeader Grid">
         <div className="MaxWidth Grid">
           <h1 className="PageHeadline">
-            <span className="PageHeadline-Small">HELLO I'M</span>
+            <span className="PageHeadline-Small">ABOUT</span>
             ANDREW
           </h1>
         </div>
@@ -52,27 +52,32 @@ export default function Home() {
               </div> */}
 
             <div className="Card FirstCard">
-              <h3>ABOUT ME</h3>
+              <h3>HELLO, STRANGER!</h3>
               <p>
-                Hellllllo! I’m Andrew. You found my cool website which I spent
-                way too long making because I wanted to. I’m an entrepreneur,
-                whose primary avocation since I was a wee boy has been making
-                cool websites for way too long because I wanted to.
+                Welcome to asuth.com. I built this website so you can get to
+                know me a bit.
               </p>
+
               <p>
-                I made Quizlet, an educational software company serving millions
-                of students and teachers worldwide. Quizlet, at its best, is
-                transformative for students’ confidence. It gives them a system
-                for learning that helps them come prepared to whatever class
-                they’re taking, and thus raises their own expectations of what
-                they can achieve.
+                I founded Quizlet, an educational software company serving
+                millions of students and teachers worldwide. Students tell
+                Quizlet what they’re learning, and Quizlet gives them
+                flashcards, games, and other activities that help them master
+                their material. Using Quizlet can be a transformative experience
+                for students because it gives them confidence in themselves as
+                learners. After working on Quizlet from age 15 to age 30, I left
+                the company in 2020.
               </p>
+
               <p>
-                After many years growing Quizlet, I left to work on new
-                bullshit. What that is yet isn’t exactly clear. But you’re gonna
-                be dazzled by it.
+                I’m now in a fun, exploratory phase of my life. I’m curious
+                about many things, and I’m taking my time to indulge that
+                curiosity now.
               </p>
-              <h3>My Story</h3>
+            </div>
+            <div className="Card RightCard">
+              <h3>Inventing Quizlet</h3>
+
               <p>
                 One night, when I was 15-years-old, my French teacher gave me an
                 impossible assignment: learn 111 French animals and their
@@ -83,6 +88,7 @@ export default function Home() {
                 did this, and got 100% on that test. From there, I shared the
                 tool with friends and gave it
               </p>
+
               <p>
                 In 2008, I graduated from high school and went off to MIT.
                 Studying at MIT was like drinking from a firehose, but I loved
@@ -92,6 +98,11 @@ export default function Home() {
                 rubbed off on me. I continued to work on Quizlet, sacrificing
                 sleep along the way.
               </p>
+            </div>
+
+            <div className="Card LeftCard">
+              <h3>Building the Company</h3>
+
               <p>
                 In 2011, I left MIT to move to San Francisco and focus on
                 Quizlet. It was a wrenching decision to leave before graduating,
@@ -104,20 +115,16 @@ export default function Home() {
                 of millions of monthly users, we built revenue to make it
                 sustainable, and we made the product even more useful for
                 learning. I was proud to lead the team along with a great
-                executive team.
+                executive team. After working on Quizlet for fifteen years, I
+                decided it was time to take a breath and think about what I
+                wanted to do next.
               </p>
-              <p>
-                When I reached my 30th birthday and my 14-year anniversary of
-                working on Quizlet, I decided it was time to take a breath and
-                think about what I wanted to do next.
-              </p>
-              <p>
-                My experience taught me many things. It taught me the value of
-                tough feedback, of high expectations, of sweating the details.
-                Through Quizlet, I met teachers and visited classrooms in
-                Germany, South Africa, Brazil, China, Finland, in addition to
-                many cities across the U.S..
-              </p>
+            </div>
+
+            <div className="Card RightCard">
+              <h3>The New Andrew</h3>
+
+              <p>INSERT CONTENT HERE. HA.</p>
             </div>
           </div>
         </div>
@@ -150,19 +157,10 @@ export default function Home() {
           overflow: auto;
         }
 
-        .Card {
-          background: #fff;
-          border: 28px solid #f2f1a0;
-          padding: 50px;
-          width: 66%;
-          position: relative;
-          box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.1);
-          margin-bottom: 50px;
-          font: 18px/1.6 var(--body-font);
-        }
-
         h3 {
-          font: 40px var(--headline-font);
+          font: 28px var(--headline-font);
+          text-transform: uppercase;
+          margin-bottom: 20px;
         }
 
         .CenterCard {
@@ -185,16 +183,14 @@ export default function Home() {
 
         .RightCard {
           float: right;
-          transform: rotate(2deg);
         }
 
         .LeftCard {
           float: left;
-          transform: rotate(-1deg);
         }
 
         p {
-          margin-bottom: 40px;
+          margin-bottom: 30px;
         }
 
         p:last-child {
