31d5382e3b164af454884d7b496a23b5df0a0679
Andrew Sutherland <asuth@asuth.com>
2024-07-12 23:29:15 -0700

    format page

--- pages/memorial.tsx
+++ pages/memorial.tsx
@@ -10,16 +10,19 @@ export default function () {
             <div className="Card  CenterCard">
               <h3>Howard Sutherland Memorial</h3>
               <p>
-                Howard's Memorial will be held at 10am on Saturday, July 13,
-                2024 at the{" "}
+                Howard's Memorial will be held at 10am Pacific Time on Saturday,
+                July 13, 2024 at the{" "}
                 <a href="https://www.berkeleycityclub.com">
                   Berkeley City Club
                 </a>
                 . All who knew Howard are welcome to attend.
               </p>
               <p>
-                We will be live-streaming the service. This page will be updated
-                with a link to the stream.
+                <a href="https://us02web.zoom.us/j/84868950583?pwd=AniHTvmbDi3nv5MkpHf7MTSrGodaiM.1">
+                  Click here
+                </a>{" "}
+                to view the livestream via Zoom. We aim to start the formal
+                portion of the service at 10:45am Pacific Time.
               </p>
             </div>
           </div>
