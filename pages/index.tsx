18611c2040a7003d2acc887ef000f52f6c2ec05f
Andrew Sutherland <asuth@asuth.com>
2025-11-15 15:25:34 -0800

    fix transition

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import { Component } from "react";
+import Meta from "../components/meta";
 
 export default class HomepageStub extends Component<{}, {}> {
   render() {
-    return <></>;
+    return (
+      <>
+        <Meta
+          title="Andrew Sutherland"
+          description="On the lookout for wonder"
+        />
+        <style jsx>{`
+          :global(:root) {
+            --bg-color: var(--lightBlue);
+            --dark-bg-color: var(--darkLightBlue);
+          }
+        `}</style>
+      </>
+    );
   }
 }
